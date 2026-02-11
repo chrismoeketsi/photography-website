ec33403b89835697de0ffe2a87255fc60b384b30
Chris Moeketsi <chrismoeketsi@gmail.com>
2026-02-11 18:59:56 +0200

    footer commit

--- src/components/Common/Footer/index.tsx
+++ src/components/Common/Footer/index.tsx
@@ -36,18 +36,9 @@ const Footer = () => {
       <FooterBottomSection>
         <p>
           <span>
-            © Replicated by <Link href="#">Temitayo</Link>{' '}
+            © All rights reserved
           </span>
-          <svg
-            width="15"
-            height="2"
-            viewBox="0 0 15 2"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <rect y="0.5" width="15" height="1" fill="black" />
-          </svg>
-          <span>Powered by Nextjs🚀</span>
+  
         </p>
 
         <FooterSocialsLinks>
